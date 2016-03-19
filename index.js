a11c61f13cde9f0b2de41932f9f2a98fd20a3e4b
mshr <mshr@localhost.localdomain>
2016-03-20 07:00:12 +0900

    slack-apiの導入

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var fs         = require('fs');
-var RtmClient  = require('@slack/client').RtmClient;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+var fs            = require('fs');
+var RtmClient     = require('@slack/client').RtmClient;
+var RTM_EVENTS    = require('@slack/client').RTM_EVENTS;
+var Slack         = require('slack-api');
 
 var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 var token  = config.token || '';
@@ -9,5 +10,13 @@ var rtm = new RtmClient(token);
 rtm.start();
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(msg) {
-  console.log('Message:', msg);
+  var options = {
+    token: token,
+    channel: msg.channel,
+    text: msg.text,
+    username: 'BOT'
+  };
+  Slack.chat.postMessage(options, function (error, data) {
+    console.log(data);
+  });
 });
